3e99afcbeb29a1b326d14f56bc741dc1625260fc
johnnyleba <495382353@qq.com>
2024-08-06 19:43:09 +0800

    1

--- VncDemonNFramework.framework/vncwww/novnc/include/language.js
+++
@@ -1,6 +0,0 @@
-var unlockTranslate = ['解锁', 'Unlock'];
-var lockTranslate = ['上锁', 'Lock'];
-var languageTranslate = ['中文', 'English'];
-var refreshTranslate = ['刷新', 'Refresh'];
-var confirmTranslate = ['确认', 'Confirm'];
-var inputPasswordTranslate = ['请输入密码：', 'Password Required:'];
